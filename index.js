b987efaf5effb5f708de2b653ff1e5aabba53d42
unknown <cubby.riles@gmail.com>
2019-12-09 22:03:32 -0700

    hello!!!

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express');
+const massive = require('massive');
+
+const app = express()
+
+const {SERVER_PORT, CONNECTION_STRING } = process.env
+
+massive(CONNECTION_STRING).then(dbInstance=> {
+  app.set('db', dbInstance)
+})
+  .catch(err=> console.log(err))
+
+app.use(express.json())
+
+app.listen(SERVER_PORT,  () => {
+  console.log(`Server listening on port ${SERVER_PORT}.`);
+}
